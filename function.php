a916ebf66f951f6c7fc15a0f6a2423651b502bfc
Umesh <benim.ub@gmail.com>
2021-07-14 00:17:44 +0545

    Update function.php

--- function.php
+++ function.php
@@ -1,15 +1,15 @@
 <?php
-define( ‘CF7_COUNTER’, ‘cf7-counter’ );
+define( 'CF7_counter', 'cf7-counter' );
 function cf7dtx_counter(){
-$val = get_option( CF7_COUNTER, 0) + 1; //Increment the current count
-return $val;
+    $val = get_option( CF7_counter, 0) + 1; //Increment the current count
+    return $val;
 }
-add_shortcode(‘CF7_counter’, ‘cf7dtx_counter’);
+add_shortcode('cf7-counter', 'cf7dtx_counter');
 //Action performed when the mail is actually sent by CF7
 function cf7dtx_increment_mail_counter(){
-$val = get_option( CF7_COUNTER, 0) + 1; //Increment the current count
-update_option(CF7_COUNTER, $val); //Update the settings with the new count
+    $val = get_option( CF7_counter, 0) + 1; //Increment the current count
+    update_option(CF7_counter, $val); //Update the settings with the new count
 
 }
-add_action(‘wpcf7_mail_sent’, ‘cf7dtx_increment_mail_counter’);
+add_action('wpcf7_mail_sent', 'cf7dtx_increment_mail_counter');
 ?>
